9457a3fbb96561a148ac0bdceb965e1db605289c
ambhar <amol.freelancing@gmail.com>
2022-05-13 18:28:58 +0530

    fetching data using react-query lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import styled from 'styled-components';
+
+
+
+const Heading = styled.h1`
+  text-align : center;
+`;
+
 
 function App() {
+
+  async function fetchPosts() {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos/')
+    return data
+  }
+
+
+  const { data, error, isError, isLoading } = useQuery('posts', fetchPosts)
+  // first argument is a string to cache and track the query result
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (isError) {
+    return <div>Error! {error.message}</div>
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading>Posts</Heading>
+      {
+        data.map((post) => {
+          return(
+            <div>
+              <div>{post.title}</div>
+              <img src={post.url} alt={post.thumbnailUrl}/>
+            </div>
+          ); 
+        })
+      }
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { QueryClient, QueryClientProvider } from "react-query";
+
+
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+const queryClient = new QueryClient();
+
+
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
